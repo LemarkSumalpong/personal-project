2ed106b4eafb3bc6b4b24eba70681f3ff177ce98
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-08-26 22:02:22 +0800

    skills

---
+++ src/components/SkillsSection.jsx
@@ -0,0 +1,16 @@
+const skills = [
+    {name: "HTML/CSS", level: "Expert", category: "Frontend"},
+    {name: "JavaScript", level: "Expert", category: "Frontend"},
+    {name: "React", level: "Advanced", category: "Frontend"},
+
+];
+
+export const SkillsSection = () => {
+    return 
+        <section
+        id="skills"
+        className="py-24 px-4  relative "
+        >
+
+        </section>;
+};
