c566c68f58ffce74cd92e8355ceaed22ac8a9029
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-08-27 20:53:25 +0800

    added the contact section

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,5 +1,11 @@
 export const ContactSection = () => {
-    return (
-        <section></section>
-    )
-}
+  return (
+    <section id="contact" className="py-24 px-4 relative bg-secondary/30">
+      <div className="container mx-auto max-w-5xl">
+        <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
+          Connect with <span className="text-primary">Me</span>
+        </h2>
+      </div>
+    </section>
+  );
+};
