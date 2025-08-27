6d43acd14d8f7548a549c2245f9fa1551ab43ad4
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-08-27 17:46:54 +0800

    added the pdf cv

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -21,7 +21,9 @@ export const AboutSection = () => {
                 Get in Touch
               </a>
               <a
-                href=""
+                href="/Sumalpong_CV.pdf"
+                target='_blank'
+                rel='noopener noreferrer'
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -1,16 +1,22 @@
 const skills = [
     {name: "HTML/CSS", level: "Expert", category: "Frontend"},
     {name: "JavaScript", level: "Expert", category: "Frontend"},
-    {name: "React", level: "Advanced", category: "Frontend"},
+    {name: "React.js", level: "Advanced", category: "Frontend"},
+    {name: "Vue.js", level: "Intermediate", category: "Frontend"},
+
+
+    {name: "Github", level: "Advanced", category: "Tools"},
+    {name: "Figma", level: "Intermediate", category: "Tools"},
+    {name: "VS Code", level: "Expert", category: "Tools"},
 
 ];
 
 export const SkillsSection = () => {
-    return 
+    return (
         <section
         id="skills"
-        className="py-24 px-4  relative "
+        className="py-24 px-4 relative bg-secondary/30 " 
         >
 
-        </section>;
+        </section>);
 };
